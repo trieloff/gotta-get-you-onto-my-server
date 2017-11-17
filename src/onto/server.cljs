c6fd6756398216aafc85ee7fa379b2fc5680f2ea
Linux User <lars@trieloff.net>
2017-11-17 09:38:26 +0000

    require httpurr

--- src/onto/server.cljs
+++ src/onto/server.cljs
@@ -1,4 +1,7 @@
-(ns onto.server)
-
+(ns onto.server
+  (:require [httpurr.client :as http]
+            [httpurr.client.node :refer [client]]))
+ 
 (defn main [args]
+  (println "hello")
   (+ 1 1))
