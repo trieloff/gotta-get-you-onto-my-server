03d4b957ef63349536a14502311d64a95bc98668
Lars Trieloff <trieloff@adobe.com>
2017-11-19 17:47:59 +0100

    get snapshots

--- src/onto/server.cljs
+++ src/onto/server.cljs
@@ -1,7 +1,18 @@
 (ns onto.server
   (:require [httpurr.client :as http]
+            [promesa.core :as p]
             [httpurr.client.node :refer [client]]))
- 
+
+(defn get-snapshots [token]
+  (-> (p/then (http/get client "https://api.digitalocean.com/v2/snapshots"
+                         {:headers { "Content-Type" "application/json"
+                                     "Authorization" (str "Bearer" " " token)}})
+               (fn [resp]
+                 (:snapshots (js->clj (.parse js/JSON (:body resp)) :keywordize-keys true))))
+       (p/catch (fn [err]
+                  (println err)
+                  (identity nil)))))
+
 (defn main [args]
-  (println "hello")
-  (+ 1 1))
+  (println "hello" args)
+  (p/then (get-snapshots args) #(println %)))
