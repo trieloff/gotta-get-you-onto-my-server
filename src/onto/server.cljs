d4f1e0ebe0af665df4b9677a618c7eee34b311b1
Lars Trieloff <trieloff@adobe.com>
2017-11-19 20:37:01 +0100

    create new droplet

--- src/onto/server.cljs
+++ src/onto/server.cljs
@@ -5,14 +5,32 @@
 
 (defn get-snapshots [token]
   (-> (p/then (http/get client "https://api.digitalocean.com/v2/snapshots"
-                         {:headers { "Content-Type" "application/json"
-                                     "Authorization" (str "Bearer" " " token)}})
+                        {:headers { "Content-Type" "application/json"
+                                    "Authorization" (str "Bearer" " " token)}})
                (fn [resp]
                  (:snapshots (js->clj (.parse js/JSON (:body resp)) :keywordize-keys true))))
        (p/catch (fn [err]
                   (println err)
                   (identity nil)))))
+(defn encode
+  [request]
+  (update request :body #(js/JSON.stringify (clj->js %))))
+
+(defn new-droplet [token snapshot]
+  (p/then (http/post client "https://api.digitalocean.com/v2/droplets"
+                     (encode {:headers {"Content-Type" "application/json"
+                                        "Authorization" (str "Bearer" " " token)}
+                              :body  {:name "1.do.99productrules.com",
+                                      :region "fra1"
+                                      :size "512mb"
+                                      :image snapshot
+                                      :backups false
+                                      :ipv6 false}}))
+            (fn [resp]
+              (js->clj (.parse js/JSON (:body resp)) :keywordize-keys true))))
 
 (defn main [args]
   (println "hello" args)
-  (p/then (get-snapshots args) #(println %)))
+  (-> (get-snapshots args)
+      (p/then #(new-droplet args (:id (first %1))))
+      (p/then #(println %1))))
