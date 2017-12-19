a16bc897ee6d03a13652871029b4cbf797522db9
Lars Trieloff <trieloff@adobe.com>
2017-12-19 18:34:42 +0000

    don't show output

--- build/deploy.js
+++ build/deploy.js
@@ -4,7 +4,6 @@ let code = 0;
 
 $.ls('*.zip').forEach(file => {
   const name = file.replace(/\.zip$/, "");
-  $.echo("wsk --apihost " + process.argv[2] + " --auth " + process.argv[3] + " action update " + name + " " + file + " --kind nodejs:6 --web true");
   code += $.exec("wsk --apihost " + process.argv[2] + " --auth " + process.argv[3] + " action update " + name + " " + file + " --kind nodejs:6 --web true").code;
 });
 
