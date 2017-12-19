648f20d0dba98942bbb08fca67ec7f770a9af587
Lars Trieloff <trieloff@adobe.com>
2017-12-19 16:09:11 +0000

    fail the build if deployment fails

--- build/deploy.js
+++ build/deploy.js
@@ -1,6 +1,11 @@
 var $ = require('shelljs');
 
+let code = 0;
+
 $.ls('*.zip').forEach(file => {
   const name = file.replace(/\.zip$/, "");
-  $.exec("wsk --apihost " + process.argv[2] + " --auth " + process.argv[3] + " action update " + name + " " + file + " --kind nodejs:6 --web true");
+  $.echo("wsk --apihost " + process.argv[2] + " --auth " + process.argv[3] + " action update " + name + " " + file + " --kind nodejs:6 --web true");
+  code += $.exec("wsk --apihost " + process.argv[2] + " --auth " + process.argv[3] + " action update " + name + " " + file + " --kind nodejs:6 --web true").code;
 });
+
+$.exit(code);
